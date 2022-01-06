080a817fb103170b3e8979b700b14eff5ebe076d
soumya3196 <soumyam9631@gmail.com>
2022-01-06 10:47:45 +0530

    First Commit

---
+++ src/main/java/com/example/KubeSpringDock/KubeSpringDockApplication.java
@@ -0,0 +1,21 @@
+package com.example.KubeSpringDock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@RestController
+public class KubeSpringDockApplication {
+	
+	@GetMapping("/message")
+	public String displayMessage(){
+		return "Congratulation you successfully deployed your application to kubernetes !!";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(KubeSpringDockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/KubeSpringDock/KubeSpringDockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.KubeSpringDock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KubeSpringDockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
